202f559936f46b93c83b9122dd48c01499d53021
Clayver Alexssander <aclayver100@gmail.com>
2025-11-22 03:17:56 -0300

    Feat: Criação das Janelas de Gerenciamento de Alunos
    - Implementa cadastro de Alunos no sistema
    - Implementa edição de informções de alunos.
    - Implementa pesquisa de alunos por nome

--- backend/routes/alunos.js
+++ backend/routes/alunos.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const db = require("../db_config.js");
 
 
-router.get("/buscar", async (req, res) => {
-    const { nome } = req.query;
+router.get("/pesquisar/:nome", async (req, res) => {
+    const { nome } = req.params;
 
     if(!nome){return res.status(400).json({ success: false, message: "Nome do aluno é obrigatório." });}
 
@@ -42,5 +42,63 @@ router.get("/frequencia/:idAluno", async (req, res) => {
     }
  });
 
+router.put("/editar/:id", async (req, res) => {
+  const { id } = req.params;
+  const {
+    nome, cidade, tipo_bancaria, telefone,
+    bolsista, email, ativo, nivel, id_turma
+  } = req.body;
+
+  try{
+    const [resultados] = await db.query("CALL sp_edita_aluno(?,?,?,?,?,?,?,?,?,?);",
+      [id, nome, cidade, tipo_bancaria, telefone, bolsista, email, ativo, nivel, id_turma]
+    );
+
+    const valor = resultados?.[0]?.[0]?.resultado;
+    if (valor === 1) return res.json({ success: true, message: "Aluno atualizado."});
+    if (valor === 2) return res.status(400).json({ success: false, message: "Email já existe ou turma inválida."});
+    return res.status(500).json({ success: false, message: "Erro inesperado."});
+  }catch(err){
+    console.error(err);
+    return res.status(500).json({ success:false, message: err.message });
+  }
+});
+
+router.get("/turmas", async (req, res) => {
+    try{
+        const [turmas] = await db.query("call sp_lista_turmas();");
+        res.json({success: true, data: turmas[0]});
+    }catch(err){
+        console.error(err);
+        res.status(500).json({success: false, message: err.message});
+    }
+})
+
+router.post("/matricular", async (req, res) => {
+    const { nome, email, cidade, bolsista, nivel, telefone, tipo_bancaria, id_turma } = req.body;
+    if(!nome || !email || !cidade || !bolsista || !nivel || !telefone || !tipo_bancaria || !id_turma){
+        return res.status(400).json({ resultado: 0, message: "Todos os campos são obrigatórios."});
+    }
+
+    try{
+        const [resultados] = await db.query("call sp_matricula_aluno(?,?,?,?,?,?,?,?);",
+            [nome, cidade, tipo_bancaria, telefone, bolsista, email, nivel, id_turma]
+        );
+
+        const resultado = resultados?.[0]?.[0]?.resultado;
+
+        if(resultado === 1){
+            res.status(201).json({ resultado: 1 , message: "Aluno matriculado com sucesso."});
+        }else if(resultado === 2){
+            res.status(400).json({ resultado: 2, message: "Email já existe ou turma inválida."});
+        }else{
+            res.status(500).json({ resultado: 0, message: "Erro inesperado na matrícula."});
+        }
+
+    }catch(erro){
+        console.error("Erro ao matricular aluno:", erro);
+        res.status(500).json({erro: "Erro ao matricular aluno."});
+    }
+});
 
 module.exports = router;
