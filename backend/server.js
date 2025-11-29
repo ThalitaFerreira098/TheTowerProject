c26cce74889f7e991a47cbcaa52d8a0924abb270
Clayver Alexssander <aclayver100@gmail.com>
2025-11-29 03:48:23 -0300

    Feat: Implementa novas dependencias
    - Novas dependencias instaladas no projeto para desenvolvimento da feature  de cadastro de material didático.

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const aulasRoutes=  require("./routes/aulas");
 const presencaRoutes = require("./routes/presencas");
 const alunosRoutes = require("./routes/alunos");
 const cadastraMaterialRoutes = require("./routes/cadastra_material.js");
+const financasRouter = require('./routes/financas');
+const aulasDemonstrativasRouter = require('./routes/aulas_demonstrativas');
 
 const app = express();
 const PORT = 3000;
@@ -21,6 +23,9 @@ app.use("/api/aulas", aulasRoutes);
 app.use("/api/presencas", presencaRoutes );
 app.use("/api/alunos", alunosRoutes);
 app.use("/api/cadastra-material", cadastraMaterialRoutes);
+app.use('/api/financas', financasRouter);
+app.use('/api/aula-demonstrativas', aulasDemonstrativasRouter);
+
 
 
 
