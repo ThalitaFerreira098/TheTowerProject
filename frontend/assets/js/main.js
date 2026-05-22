20f6956185dd2326a5bda66e70cc185678edf382
Jheniffer Aparecida <jheniffera457@gmail.com>
2026-05-22 20:03:02 -0300

    Feat: Implementação de sessão no projeto inteiro

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if(result.success){
                 localStorage.setItem("user", JSON.stringify(result.user));
-                window.location.href = 'home.html';
+                window.location.href = 'http://localhost:3000/frontend/home.html';
             }else{
                 error.textContent = result.message || "Usuário ou senha incorretos.";
             }
